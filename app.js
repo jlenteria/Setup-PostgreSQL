1bddf44e2cda434ca9fab842c89157d5d5b5bfba
Jonel Lenteria <jlenteria98@gmail.com>
2022-03-30 17:31:50 +0800

    cleanup

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const sequelize = require('./config/sequelize.js')
-const PORT = 3000
-
-sequelize.verifiy()
-
-app.listen(PORT, () => console.log('Server running at PORT: ', PORT))
